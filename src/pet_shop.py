93cfea1abd11fe89b6b22ad602f7713bbb6c0dcd
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 11:35:44 +0100

    Added first two functions, pass=2, skipped=21

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,8 @@
 # WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    cash = pet_shop["admin"]["total_cash"]
+    return cash
