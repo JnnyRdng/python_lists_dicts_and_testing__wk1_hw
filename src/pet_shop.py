7b45794ea3c5f3daa2a8aa4d208539b998b7e308
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 16:09:29 +0100

    Formatted file, removed unnecessary line from 'get_total_cash()' and expanded on comment in 'increase_pets_sold()' after experimentation

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,24 +1,31 @@
 # WRITE YOUR FUNCTIONS HERE
 
+
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
+
 def get_total_cash(pet_shop):
-    cash = pet_shop["admin"]["total_cash"]
-    return cash
+    return pet_shop["admin"]["total_cash"]
+
 
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] += cash
 
+
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
+
 def increase_pets_sold(pet_shop, increase):
     # can i call get_pets_sold(pet_shop) and increase that?
+    # no, get_pets_sold(pet_shop) returns an int, not a reference to the dictionary itself. increasing that returned value will not cause the dictionary to update
     pet_shop["admin"]["pets_sold"] += increase
+
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
 
 def get_pets_by_breed(pet_shop, breed):
     pets = []
@@ -27,29 +34,37 @@ def get_pets_by_breed(pet_shop, breed):
             pets.append(pet)
     return pets
 
+
 def find_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
             return pet
 
+
 def remove_pet_by_name(pet_shop, name):
     pet_to_remove = find_pet_by_name(pet_shop, name)
     pet_shop["pets"].remove(pet_to_remove)
 
+
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
+
 
 def get_customer_cash(customer):
     return customer["cash"]
 
+
 def remove_customer_cash(customer, amount):
     customer["cash"] -= amount
+
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
 
+
 def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
+
 
 # optional test functions
 def customer_can_afford_pet(customer, new_pet):
@@ -66,4 +81,5 @@ def sell_pet_to_customer(pet_shop, pet, customer):
             price = pet["price"]
             remove_customer_cash(customer, price)
             add_or_remove_cash(pet_shop, price)
-    
+            # not tested, but can't sell the pet twice!
+            remove_pet_by_name(pet_shop, pet["name"])
