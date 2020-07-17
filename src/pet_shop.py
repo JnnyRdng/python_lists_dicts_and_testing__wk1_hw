549825d26c693f95ea2e9c5e7604016aa6f8a323
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 12:14:51 +0100

    9th function passes, pass=12, skipped=11

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -31,3 +31,7 @@ def find_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
             return pet
+
+def remove_pet_by_name(pet_shop, name):
+    pet_to_remove = find_pet_by_name(pet_shop, name)
+    pet_shop["pets"].remove(pet_to_remove)
