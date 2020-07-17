6c5b086cb2a944c42574d2b67ffdf2f2e9e63a59
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 12:02:39 +0100

    Added 7th function, pass=9, skipped=14

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -19,3 +19,10 @@ def increase_pets_sold(pet_shop, increase):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets.append(pet)
+    return pets
