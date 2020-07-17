eedea6311a1b9bbf3b8c5f73b9d3e5c4f25d378c
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 11:56:22 +0100

    Added fifth function, pass=6, skipped=17
    Code comment in function explains idea best left for exploration and refactoring later

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -12,3 +12,8 @@ def add_or_remove_cash(pet_shop, cash):
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, increase):
+    # can i call get_pets_sold(pet_shop) and increase that?
+    pet_shop["admin"]["pets_sold"] += increase
+
