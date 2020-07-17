3f88aa37e0ecb766e4eddb8bd7b2ce16c79db241
Jonny Reading <jonnyreading@gmail.com>
2020-07-17 14:41:14 +0100

    Modifed function to check for non-existant pet before selling, pass=22, skipped=1

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -58,10 +58,10 @@ def customer_can_afford_pet(customer, new_pet):
 
 # integration tests
 def sell_pet_to_customer(pet_shop, pet, customer):
-    add_pet_to_customer(customer, pet)
-    increase_pets_sold(pet_shop, 1)
-    price = pet["price"]
-    remove_customer_cash(customer, price)
-    add_or_remove_cash(pet_shop, price)
-
+    if pet is not None:
+        add_pet_to_customer(customer, pet)
+        increase_pets_sold(pet_shop, 1)
+        price = pet["price"]
+        remove_customer_cash(customer, price)
+        add_or_remove_cash(pet_shop, price)
     
